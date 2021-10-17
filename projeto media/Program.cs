74490699972db4ab36808643f4cc55c29b3e9d1f
USER <USER@USER-PC>
2021-10-17 14:25:17 -0300

    Adicionar arquivos de projeto.

---
+++ projeto media/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace projeto_media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2;
+            double media;
+            Console.WriteLine("DIGITE A PRIMEIRA NOTA : ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("DIGITE A SEGUNDA NOTA :");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                Console.WriteLine("ALUNO APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("ALUNO REPROVADO");
+            }
+            Console.ReadKey();
+        }
+    }
+}
